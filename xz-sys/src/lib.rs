d1286e028d8a5e4f520f2741dc5ec55c5319ef7e
Hyeon Kim <simnalamburt@gmail.com>
2015-02-19 04:30:17 +0900

    xz-sys: vli.h

--- xz-sys/src/lib.rs
+++ xz-sys/src/lib.rs
@@ -5,6 +5,10 @@ use libc::*;
 //
 // lzma/version.h
 //
+pub const LZMA_VERSION_STABILITY_ALPHA: c_int = 0;
+pub const LZMA_VERSION_STABILITY_BETA: c_int = 1;
+pub const LZMA_VERSION_STABILITY_STABLE: c_int = 2;
+
 extern {
     pub fn lzma_version_number() -> uint32_t;
     pub fn lzma_version_string() -> *const c_char;
@@ -14,8 +18,9 @@ extern {
 // lzma/base.h
 //
 #[repr(C)]
-pub type lzma_boot = c_uchar;
+pub type lzma_bool = c_uchar;
 
+// Note: https://github.com/rust-lang/rust/issues/10292
 #[repr(C)]
 pub type lzma_reserved_enum = c_int;
 pub const LZMA_RESERVED_ENUM: lzma_reserved_enum = 0;
@@ -95,6 +100,27 @@ extern {
 }
 
 //
+// lzma/vli.h
+//
+pub const LZMA_VLI_MAX: uint64_t = std::u64::MAX / 2;
+pub const LZMA_VLI_UNKNOWN: uint64_t = std::u64::MAX;
+pub const LZMA_VLI_BYTES_MAX: c_int = 9;
+
+#[repr(C)]
+pub type lzma_vli = uint64_t;
+
+#[inline]
+pub fn lzma_vli_is_valid(vli: lzma_vli) -> bool {
+    vli <= LZMA_VLI_MAX || vli == LZMA_VLI_UNKNOWN
+}
+
+extern {
+    pub fn lzma_vli_encode(vli: lzma_vli, vli_pos: *mut size_t, out: *mut uint8_t, out_pos: *mut size_t, out_size: size_t) -> lzma_ret;
+    pub fn lzma_vli_decode(vli: *mut lzma_vli, vli_pos: *mut size_t, in_: *const uint8_t, in_pos: *mut size_t, in_size: size_t) -> lzma_ret;
+    pub fn lzma_vli_size(vli: lzma_vli) -> uint32_t;
+}
+
+//
 // lzma/check.h
 //
 pub use lzma_check::*;
@@ -105,6 +131,51 @@ pub enum lzma_check {
     LZMA_CHECK_CRC32    = 1,
     LZMA_CHECK_CRC64    = 4,
     LZMA_CHECK_SHA256   = 10
+}
+
+pub const LZMA_CHECK_ID_MAX: c_int = 15;
+
+extern {
+    pub fn lzma_check_is_supported(check: lzma_check) -> lzma_bool;
+    pub fn lzma_check_size(check: lzma_check) -> uint32_t;
+}
+
+pub const LZMA_CHECK_SIZE_MAX: c_int = 64;
+
+extern {
+    pub fn lzma_crc32(buf: *const uint8_t, size: size_t, crc: uint32_t) -> uint32_t;
+    pub fn lzma_crc64(buf: *const uint8_t, size: size_t, crc: uint64_t) -> uint64_t;
+    pub fn lzma_get_check(strm: *const lzma_stream) -> lzma_check;
+}
+
+//
+// lzma/filter.h
+//
+pub const LZMA_FILTERS_MAX: c_int = 4;
+
+#[repr(C)]
+pub struct lzma_filter {
+    pub id: lzma_vli,
+    pub options: *mut c_void,
+}
+
+extern {
+    pub fn lzma_filter_encoder_is_supported(id: lzma_vli) -> lzma_bool;
+    pub fn lzma_filter_decoder_is_supported(id: lzma_vli) -> lzma_bool;
+    pub fn lzma_filters_copy(src: *const lzma_filter, dest: *mut lzma_filter, allocator: *const lzma_allocator) -> lzma_ret;
+    pub fn lzma_raw_encoder_memusage(filters: *const lzma_filter) -> uint64_t;
+    pub fn lzma_raw_decoder_memusage(filters: *const lzma_filter) -> uint64_t;
+    pub fn lzma_raw_encoder(strm: *mut lzma_stream, filters: *const lzma_filter) -> lzma_ret;
+    pub fn lzma_raw_decoder(strm: *mut lzma_stream, filters: *const lzma_filter) -> lzma_ret;
+    pub fn lzma_filters_update(strm: *mut lzma_stream, filters: *const lzma_filter) -> lzma_ret;
+    pub fn lzma_raw_buffer_encode(filters: *const lzma_filter, allocator: *const lzma_allocator, in_: *const uint8_t, in_size: size_t, out: *mut uint8_t, out_pos: *mut size_t, out_size: size_t) -> lzma_ret;
+    pub fn lzma_raw_buffer_decode(filters: *const lzma_filter, allocator: *const lzma_allocator, in_: *const uint8_t, in_pos: *mut size_t, in_size: size_t, out: *mut uint8_t, out_pos: *mut size_t, out_size: size_t) -> lzma_ret;
+    pub fn lzma_properties_size(size: *mut uint32_t, filter: *const lzma_filter) -> lzma_ret;
+    pub fn lzma_properties_encode(filter: *const lzma_filter, props: *mut uint8_t) -> lzma_ret;
+    pub fn lzma_properties_decode(filter: *mut lzma_filter, allocator: *const lzma_allocator, props: *const uint8_t, props_size: size_t) -> lzma_ret;
+    pub fn lzma_filter_flags_size(size: *mut uint32_t, filter: *const lzma_filter) -> lzma_ret;
+    pub fn lzma_filter_flags_encode(filter: *const lzma_filter, out: *mut uint8_t, out_pos: *mut size_t, out_size: size_t) -> lzma_ret;
+    pub fn lzma_filter_flags_decode(filter: *mut lzma_filter, allocator: *const lzma_allocator, in_: *const uint8_t, in_pos: *mut size_t, in_size: size_t) -> lzma_ret;
 }
 
 //
