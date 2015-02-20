4cfde4ab2924af97083fae8f4d78f5e65117a494
Hyeon Kim <simnalamburt@gmail.com>
2015-02-20 12:26:44 +0900

    xz-sys: build.rs

--- xz-sys/build.rs
+++ xz-sys/build.rs
@@ -1,8 +1,43 @@
+#![feature(env, process)]
+#![feature(old_path)]
+
 extern crate "pkg-config" as pkg_config;
 
+use std::env;
+use std::process::Command;
+
 fn main() {
+    // Cross compile not supported yet. See #8
+    if env::var("TARGET") != env::var("HOST") { unimplemented!() }
+
+    // Use system installed liblzma if it exists, compile it manually otherwise.
     match pkg_config::find_library("liblzma") {
         Ok(_) => return,
-        Err(..) => unimplemented!()
+        Err(_) => ()
     }
+
+    let out_dir = env::var("OUT_DIR").ok().unwrap();
+    let num_jobs = env::var("NUM_JOBS").ok().unwrap();
+
+    // cd xz
+    env::set_current_dir(&Path::new("xz")).ok().unwrap();
+
+    let ret = Command::new("./configure")
+        .args(&[
+              "--disable-debug",
+              "--disable-dependency-tracking",
+              "--disable-silent-rules",
+        ])
+        .arg(&format!("--prefix={}", out_dir))
+        .status().unwrap().success();
+    assert!(ret);
+
+    let ret = Command::new("make")
+        .arg(&format!("-j{}", num_jobs))
+        .arg("install")
+        .status().unwrap().success();
+    assert!(ret);
+
+    println!("cargo:rustc-flags=-L native={}/lib", out_dir);
+    println!("cargo:rustc-flags=-l lzma");
 }
